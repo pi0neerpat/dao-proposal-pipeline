80a56d685c10acdca77acd3b96fe9a6032740d50
Miguel  de la Mata <sebidelamata@gmail.com>
2024-05-17 10:24:30 -0600

    handle not submitted error state

--- src/app/components/Proposals/index.tsx
+++ src/app/components/Proposals/index.tsx
@@ -7,6 +7,7 @@ import { ProposalType } from "@/app/types/proposal";
 import Proposal from "./Proposal";
 import { useEtherProviderContext } from "@/app/contexts/ProviderContext";
 import { ethers } from "ethers";
+import ODGovernorType from "@/app/types/ODGovernorType";
 
 
 interface ProposalsProps{
@@ -15,36 +16,102 @@ interface ProposalsProps{
 
 const Proposals:React.FC<ProposalsProps> = ({proposals}) => {
 
-    const [isSubmitted, setIsSubmitted] = useState<boolean[]>([])
+    const [proposalMetadata, setProposalMetadata] = useState<any[]>([])
 
     const { address, provider, signer, odGovernor } = useEtherProviderContext();
     
-    const fetchProposalSubmission = async (proposals, odGovernor) => {
+    // this function fetches the proposal meta data for each proposal, creates an array of them
+    // returns error if not submitted yet
+    const fetchProposalMetadata = async (
+        proposals: ProposalType[], 
+        odGovernor: ODGovernorType
+    ) => {
         if(!proposals || !odGovernor) return;
-        //console.log(proposals)
-        const submissionsPromises = proposals.map(async (proposal) => {
+        const metadataPromises = proposals.map(async (proposal) => {
             try {
                 let proposalId = proposal.proposalId;
                 console.log(proposalId)
                 //proposalId = ethers.BigNumber.from(proposalId);
-                const submitted = await odGovernor.proposals(proposalId);
-                console.log("submitted: "+  await submitted)
-                return submitted;
-            } catch (error) {
-                console.error("Error encountered fetching proposal submission states: ", error.toString());
+                const metadata = await odGovernor.proposals(proposalId);
+                console.log("metadata: "+  await metadata)
+                // handle response object
+                const {
+                    id,
+                    proposer,
+                    eta,
+                    startBlock,
+                    endBlock,
+                    forVotes,
+                    againstVotes,
+                    abstainVotes,
+                    canceled,
+                    executed,
+                  } = metadata;
+                return {
+                    id: id.toString(),
+                    proposer,
+                    eta: eta.toString(),
+                    startBlock: startBlock.toString(),
+                    endBlock: endBlock.toString(),
+                    forVotes: forVotes.toString(),
+                    againstVotes: againstVotes.toString(),
+                    abstainVotes: abstainVotes.toString(),
+                    canceled,
+                    executed,
+                };
+            } catch (error: any) {
+                // this is the case where the proposal has not been submitted
+                if(error.reason.toString() === 'Governor: unknown proposal id'){
+                    // default null response object
+                    const {
+                        id,
+                        proposer,
+                        eta,
+                        startBlock,
+                        endBlock,
+                        forVotes,
+                        againstVotes,
+                        abstainVotes,
+                        canceled,
+                        executed,
+                      } = {
+                            "id": proposal.proposalId,
+                            "proposer": "",
+                            "eta": "",
+                            "startBlock": "",
+                            "endBlock": "",
+                            "forVotes": "",
+                            "againstVotes": "",
+                            "abstainVotes": "",
+                            "canceled": "",
+                            "executed": "",   
+                      };
+                    return {
+                        id: id.toString(),
+                        proposer,
+                        eta: eta.toString(),
+                        startBlock: startBlock.toString(),
+                        endBlock: endBlock.toString(),
+                        forVotes: forVotes.toString(),
+                        againstVotes: againstVotes.toString(),
+                        abstainVotes: abstainVotes.toString(),
+                        canceled,
+                        executed,
+                    };
+                };
                 return null;
             }
         });
 
-        const submissions = await Promise.all(submissionsPromises);
-        setIsSubmitted(submissions)
+        const metadataArray = await Promise.all(metadataPromises);
+        setProposalMetadata(metadataArray)
     }
     useEffect(() => {
         if (proposals.length > 0 && odGovernor) {
-            fetchProposalSubmission(proposals, odGovernor);
+            fetchProposalMetadata(proposals, odGovernor);
           }
     },[proposals, odGovernor])
-    console.log(isSubmitted)
+    console.log(proposalMetadata)
     return(
         <ul className="proposals-list">
             <div className="proposals-header">
