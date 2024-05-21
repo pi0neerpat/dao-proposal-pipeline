836a90c36062948210d64a5d5f134f70c5f9c38e
Miguel  de la Mata <sebidelamata@gmail.com>
2024-05-21 08:44:46 -0600

    home draft

--- src/app/components/Proposals/Proposal.tsx
+++ src/app/components/Proposals/Proposal.tsx
@@ -20,82 +20,110 @@ const Proposal:React.FC<ProposalProps> = ({index}) => {
             href={`/proposal/[proposalId]`}
             as={`/proposal/${proposals[index].proposalId.toString()}`}
         >
-            <h3 className="proposal-description">
-                {proposals[index].description}
-            </h3>
-            <div className="proposal-type">
-                {proposals[index].proposalType}
+            <div className="proposal-row-one">
+                <h3 className="proposal-description">
+                    {proposals[index].description}
+                </h3>
+                <div className="proposal-type">
+                    {proposals[index].proposalType}
+                </div>
             </div>
-            <div className="proposal-id">
-                {proposals[index].proposalId.toString().slice(0,10)}...
+            <div className="proposal-row-two">
+                <div className="proposal-id">
+                    Proposal ID: {proposals[index].proposalId.toString().slice(0,10)}...
+                </div>
+                <div className="proposal-submitted">
+                    {
+                        (
+                            proposalMetadata.length > 0 &&
+                            proposalMetadata[index].proposer === ""
+                        ) ? (
+                            "Unsubmitted"
+                        ) : (
+                            "Submitted"
+                        )
+                    }
+                </div>
             </div>
-            <div className="proposal-submitted">
+            <div className="proposal-process">
                 {
-                    (
-                        proposalMetadata.length > 0 &&
-                        proposalMetadata[index].proposer === ""
-                    ) ? (
-                        "Unsubmitted"
-                    ) : (
-                        "Submitted"
-                    )
+                    proposalMetadata.length > 0 &&
+                    proposalMetadata[index].proposer !== "" &&
+                    <div className="proposal-vote-details">
+                        <h4 className="proposal-vote-detail-title">
+                            Vote Details
+                        </h4>
+                        <div className="proposal-votes-detail-labels">
+                            <div className="proposal-vote-detail-label">
+                                For
+                            </div>
+                            <div className="proposal-vote-detail-label">
+                                Against
+                            </div>
+                            <div className="proposal-vote-detail-label">
+                                Total
+                            </div>
+                        </div>
+                        <div className="proposal-vote-detail-values">
+                            <div className="proposal-votes-for">
+                                { 
+                                    (
+                                        proposalMetadata.length > 0 &&
+                                        proposalMetadata[index].proposer !== "" 
+                                    ) ? (
+                                        `${convertTokensToThousandsK(proposalMetadata[index].forVotes)}`
+                                    ) : (
+                                        ""
+                                    )
+                                }
+                            </div>
+                            <div className="proposal-votes-against">
+                                { 
+                                    (
+                                        proposalMetadata.length > 0 &&
+                                        proposalMetadata[index].proposer !== "" 
+                                    ) ? (
+                                        `${convertTokensToThousandsK(proposalMetadata[index].againstVotes)}`
+                                    ) : (
+                                        ""
+                                    )
+                                }
+                            </div>
+                            <div className="proposal-votes-total">
+                                { 
+                                    (
+                                        proposalMetadata.length > 0 &&
+                                        proposalMetadata[index].proposer !== "" 
+                                    ) ? (
+                                        `${convertTokensToThousandsK(
+                                            proposalMetadata[index].againstVotes + proposalMetadata[index].forVotes
+                                        )}`
+                                    ) : (
+                                        ""
+                                    )
+                                }
+                            </div>
+                        </div>
+                    </div>   
                 }
-            </div>
-            {
-                proposalMetadata.length > 0 &&
-                proposalMetadata[index].proposer !== "" &&
-                proposalMetadata[index].executed === true &&
-                <div className="proposal-executed">Executed</div>
-            }
-            {
-                proposalMetadata.length > 0 &&
-                proposalMetadata[index].proposer !== "" &&
-                proposalMetadata[index].cancelled === true &&
-                <div className="proposal-cancelled">Cancelled</div>
-            }
-            {
-                proposalMetadata.length > 0 &&
-                proposalMetadata[index].proposer !== "" &&
-                proposalMetadata[index].executed === false &&
-                proposalMetadata[index].cancelled === false &&
-                <div className="proposal-pending">Pending</div>
-            }
-            <div className="proposal-votes-for">
-                { 
-                    (
-                        proposalMetadata.length > 0 &&
-                        proposalMetadata[index].proposer !== "" 
-                    ) ? (
-                        `${convertTokensToThousandsK(proposalMetadata[index].forVotes)}`
-                    ) : (
-                        ""
-                    )
+                {
+                    proposalMetadata.length > 0 &&
+                    proposalMetadata[index].proposer !== "" &&
+                    proposalMetadata[index].executed === true &&
+                    <div className="proposal-executed proposal-state">Executed</div>
                 }
-            </div>
-            <div className="proposal-votes-against">
-                { 
-                    (
-                        proposalMetadata.length > 0 &&
-                        proposalMetadata[index].proposer !== "" 
-                    ) ? (
-                        `${convertTokensToThousandsK(proposalMetadata[index].againstVotes)}`
-                    ) : (
-                        ""
-                    )
+                {
+                    proposalMetadata.length > 0 &&
+                    proposalMetadata[index].proposer !== "" &&
+                    proposalMetadata[index].cancelled === true &&
+                    <div className="proposal-cancelled proposal-state">Cancelled</div>
                 }
-            </div>
-            <div className="proposal-votes-total">
-                { 
-                    (
-                        proposalMetadata.length > 0 &&
-                        proposalMetadata[index].proposer !== "" 
-                    ) ? (
-                        `${convertTokensToThousandsK(
-                            proposalMetadata[index].againstVotes + proposalMetadata[index].forVotes
-                        )}`
-                    ) : (
-                        ""
-                    )
+                {
+                    proposalMetadata.length > 0 &&
+                    proposalMetadata[index].proposer !== "" &&
+                    proposalMetadata[index].executed === false &&
+                    proposalMetadata[index].cancelled === false &&
+                    <div className="proposal-pending proposal-state">Pending</div>
                 }
             </div>
         </Link>
